9724731e8234a88e01f7df429f0a8fbd38c57a77
TaehwanGo <gth1123@naver.com>
2021-01-02 19:04:05 +0900

    #0.10 Creating first Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,15 @@
-import { people, getById } from "./db";
+import { getById, getMovies, addMovie } from "./db";
 
 const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, {id}) => { // _, args
+        movies: () => getMovies(),
+        movie: (_, {id}) => { // _, args
             // console.log("args:", args); // {id : 1}
             return getById(id);
         }
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => addMovie(name, score)
     }
 }
 
